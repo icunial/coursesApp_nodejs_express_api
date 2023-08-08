c0c7ee0a4943df0b4ef626cc742193a7cd0c09d1
Ignacio Cunial <igsecu@hotmail.com>
2023-08-08 15:01:29 -0300

    Created courses.js -> hardcoded courses information

---
+++ courses.js
@@ -0,0 +1,41 @@
+let infoCourses = {
+  coding: [
+    {
+      id: 1,
+      title: "Learn Python",
+      language: "python",
+      views: 15000,
+      level: "basic",
+    },
+    {
+      id: 2,
+      title: "Python intermediate",
+      language: "python",
+      views: 12000,
+      level: "intermediate",
+    },
+    {
+      id: 3,
+      title: "Learn Javascript",
+      language: "javascript",
+      views: 20875,
+      level: "basic",
+    },
+  ],
+  mathmatics: [
+    {
+      id: 1,
+      title: "Learn Calculation",
+      topic: "calculation",
+      views: 1500,
+      level: "basic",
+    },
+    {
+      id: 2,
+      title: "Learn algebra",
+      topic: "algebra",
+      views: 1200,
+      level: "intermediate",
+    },
+  ],
+};
