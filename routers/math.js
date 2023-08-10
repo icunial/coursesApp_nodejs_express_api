4d3ffdacae68243d23761ea16a4a9fda771a9cbb
Ignacio Cunial <igsecu@hotmail.com>
2023-08-10 11:19:23 -0300

    Updated GET mathRouter routes -> json responses

--- routers/math.js
+++ routers/math.js
@@ -3,19 +3,30 @@ const mathRouter = express.Router();
 
 const courses = require("../courses");
 
+// Gets all math courses
 mathRouter.get("/", (req, res) => {
-  res.json(courses.mathmatics);
+  res.status(200).json({
+    statusCode: 200,
+    data: courses.mathmatics,
+  });
 });
 
+// Gets all math courses filterd by topic
 mathRouter.get("/:topic", (req, res) => {
   const topic = req.params.topic;
   const results = courses.mathmatics.filter((course) => course.topic === topic);
 
   if (results.length === 0) {
-    return res.status(404).send(`Courses about ${topic} not found!`);
+    return res.status(404).json({
+      statusCode: 404,
+      msg: `Courses about ${topic} not found!`,
+    });
   }
 
-  res.json(results);
+  res.status(200).json({
+    statusCode: 200,
+    data: results,
+  });
 });
 
 module.exports = mathRouter;
